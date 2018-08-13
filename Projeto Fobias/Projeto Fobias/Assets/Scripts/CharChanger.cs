e64a64d94c0f29989e516ab19e42a009f8aaec52
Joao Rabello <rabellois@hotmail.com>
2018-08-13 00:47:23 -0300

    Sistema de Troca de Personagens
    -Sistema concluído, porém será modificado o sistema de ativar e desativar para uma pool com os 4 personagens.
    
    v0.071

--- Projeto Fobias/Projeto Fobias/Assets/Scripts/CharChanger.cs
+++ Projeto Fobias/Projeto Fobias/Assets/Scripts/CharChanger.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class CharChanger : MonoBehaviour {
 
-    public int charNumber;
     bool canChange;
-    
+    public int charNumber;
+    int lastCharNumber;
+    int tempCharNumber;
+
+    GameObject ariel;
+    GameObject clarice;
+    GameObject ezekiel;
+    GameObject pamela;
+
+    private void Awake()
+    {
+        ariel = GameObject.FindGameObjectWithTag("Ariel");
+        clarice = GameObject.FindGameObjectWithTag("Clarice");
+        ezekiel = GameObject.FindGameObjectWithTag("Ezekiel");
+        pamela = GameObject.FindGameObjectWithTag("Pamela");
+    }
+
     // Use this for initialization
-	void Start () {
+    void Start () {
         canChange = false;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,9 +34,36 @@ public class CharChanger : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if ((other.gameObject.CompareTag("Ariel") || other.gameObject.CompareTag("Ezekiel") || other.gameObject.CompareTag("Clarice") || other.gameObject.CompareTag("Pamela")))
+        if (other.gameObject.CompareTag("Ariel"))
         {
             canChange = true;
+            lastCharNumber = 0;
+            Debug.Log(lastCharNumber);
+        }
+        else
+        {
+            if (other.gameObject.CompareTag("Ezekiel"))
+            {
+                canChange = true;
+                lastCharNumber = 1;
+            }
+            else
+            {
+                if (other.gameObject.CompareTag("Clarice"))
+                {
+                    canChange = true;
+                    lastCharNumber = 2;
+                    Debug.Log(lastCharNumber);
+                }
+                else
+                {
+                    if (other.gameObject.CompareTag("Pamela"))
+                    {
+                        canChange = true;
+                        lastCharNumber = 3;
+                    }
+                }
+            }
         }
     }
 
@@ -39,13 +81,27 @@ public class CharChanger : MonoBehaviour {
         {
             case 0:
                 //Muda para Ariel
+                ariel.SetActive(true);
+                clarice.SetActive(false);
+                charNumber = 0;
+                tempCharNumber = charNumber;
+                charNumber = lastCharNumber;
+                lastCharNumber = tempCharNumber;
+                Debug.Log(charNumber);
                 break;
             case 1:
                 //Muda para Ezekiel
                 break;
             case 2:
                 //Muda para Clarice
-                Debug.Log("Muda para clarice");
+                ariel.SetActive(false);
+                clarice.SetActive(true);
+                clarice.GetComponent<SpriteRenderer>().enabled = true;
+                charNumber = 2;
+                tempCharNumber = charNumber;
+                charNumber = lastCharNumber;
+                lastCharNumber = tempCharNumber;
+                Debug.Log(charNumber);
                 break;
             case 3:
                 //Muda para Pamela
