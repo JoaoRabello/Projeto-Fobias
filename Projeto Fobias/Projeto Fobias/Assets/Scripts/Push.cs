e443cb0aae1d954bae99164ebb2f8f35c9dace2b
Joao Rabello <rabellois@hotmail.com>
2018-09-21 11:44:13 -0300

    Sistema de Empurrar
    -Possibilidade de empurrar objetos com o botão S. Por enquanto está bem cru, mas funcional.
    -Modificações diversas
    
    v0.3

---
+++ Projeto Fobias/Projeto Fobias/Assets/Scripts/Push.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour {
+
+    public bool playerTouching;
+    Transform playerT;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.S) && playerTouching)
+        {
+            transform.SetParent(playerT);
+        }
+        else
+        {
+            transform.parent = null;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Clarice") || col.gameObject.CompareTag("Ariel")){
+            playerTouching = true;
+            playerT = col.gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Clarice") || col.gameObject.CompareTag("Ariel"))
+        {
+            playerTouching = false;
+        }
+    }
+}
